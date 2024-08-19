6d5d7f52676d165f39eb27337719bc8f01ad2f9c
JulioMisson <159930590+JulioMisson@users.noreply.github.com>
2024-08-19 13:42:11 -0300

    new exercises

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 40
+console.log(`A velocidade do seu carro é ${vel}km/hr`)
+if (vel > 50){
+    console.log('Você sera multado!')
+}
+console.log('Use cinto de SEGURANÇA!')
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var suspeito ='bandido'
+console.log(` voce é um ${suspeito}`)
+if (suspeito == 'inocente') {
+    console.log(`voce passou`)
+} else {
+    console.log('voce vai PRESO!')
+}
